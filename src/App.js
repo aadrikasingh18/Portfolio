2681e3fc55db3959f4a47e603e575a6256819e73
aadrikasingh18 <aadrikas16@gmail.com>
2024-02-18 19:19:25 +0530

    Hero Section Created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
-import About from "./Components/About"
+// import About from "./Components/About"
 import Experience from "./Components/Experience"
 import Project from "./Components/Project"
 import Contact from "./Components/Contact"
@@ -13,7 +13,7 @@ function App() {
     <Router>     
       <div className='app'><Navbar /></div>
       <div className='app'><Hero /></div>
-      <div className='app'><About /></div>
+      {/* <div className='app'><About /></div> */}
       <div className='app'><Experience /></div>
       <div className='app'><Project /></div>
       <div className='app'><Contact /></div>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import './Navbar.css';
+import './Hero.css';
+import heroPic from '../images/hero.png'
 
 const Hero = () => {
   return (
     <>
-      hero
+      <section className='hero-container'>
+        {/* LEFT DIV*/}
+        <div className='hero-content-left'>
+          <h1 className='hero-title'>Hi! I'm Aadrika</h1>
+          <p className="hero-description">I am a Full Stack Developer with an experience in React.Js and Node.Js. <br></br>
+          Reach out to me </p>
+          <div className='hero-btn'>
+
+          <a href="mailto:myemail.com" className='hero-contactBtn'>CONTACT ME</a>
+          </div>
+        </div>
+        {/* RIGHT DIV */}
+        <div className='hero-content-right'>
+          <img src={heroPic} alt={"pic"} className='hero-img' />
+        </div>
+        {/* Circle for blur effect */}
+        <div className="hero-topBlur"></div>
+        <div className="hero-bottomBlur"></div>
+      </section>
     </>
   )
 }
