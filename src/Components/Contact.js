12aac6e36bbcb2cfaf1e704c02d0e8e1846b3cdf
aadrikasingh18 <aadrikas16@gmail.com>
2024-02-21 12:28:54 +0530

    Contact Section Created

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,8 +4,33 @@ import './Contact.css';
 const Contact = () => {
   return (
     <>
-      <div>
-        CONTACT
+      <div className='contact'>
+        <div className="contact-left">
+          <div className="contact-title">CONTACT
+          </div>
+        </div>
+
+        <div className="contact-right">
+          <div className="contact-icons">
+
+            <a href="https://www.linkedin.com/in/aadrika-singh-035219205/" target="_blank" className="contact-btn">
+              <i className="fab fa-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/aadrikasingh18" target="_blank" className="contact-btn">
+              <i className="fab fa-github"></i>
+            </a>
+
+            <a href="https://wa.me/6388267640" target="_blank" className="contact-btn">
+              <i className="fab fa-brands fa-whatsapp"></i>
+            </a>
+
+            <a href="https://twitter.com/aadrika_singh__" target="_blank" className="contact-btn">
+              <i className="fab fa-twitter"></i>
+            </a>
+
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/Components/Skill.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Skill.css';
+
+const Skill = () => {
+    return (
+        <div>
+            SKILL
+        </div>
+    )
+}
+
+export default Skill
+
