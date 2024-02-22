dd27287099f1d22c78b119fbef14089b3671ffdd
aadrikasingh18 <aadrikas16@gmail.com>
2024-02-22 15:03:54 +0530

    Skill Section Created

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,18 +1,72 @@
 import React from 'react'
-import './Navbar.css';
+import './Project.css';
+import projectPic from '../images/project.jpg'
 
 const Project = () => {
   return (
     <>
-      {/* <div className="card" style="width: 18rem;"> */}
-      <div className="card">
-        {/* <img className="card-img-top" src="..." alt="Card image cap" /> */}
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+      <section>
+        <div className='project'>
+          <h2 className='project-heading'>
+            PROJECTS
+          </h2>
+
+          <div className='project-container'>
+            {/* RIGHT DIV */}
+            <div className='project-box-right'>
+              <ul className='project-list'>
+                <li>
+                  <div className="pro">
+                    <p className='pro-role'>
+                      VEWRITE: A CONTENT MANAGEMENT TOOL
+                    </p>
+                    {/* <p className='pro-date'>Aug, 2023 - Oct, 2023</p> */}
+                    <p className='pro-des'>• It has been crafted to empower writers in content creation and management, all while fostering seamless collaboration with fellow writers.
+                      <br></br>
+                      • Tech stack used are ReactJs, Tailwind CSS, Firebase, and Sentry.</p>
+                    <button type="button" className="btn btn-light" id="btn">LINK</button>
+                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                  </div>
+                </li>
+
+                <li>
+                  <div className="pro">
+                    <p className='pro-role'>
+                      TEXT TO SPEECH CONVERTER
+                    </p>
+                    {/* <p className='pro-date'>July, 2023 - Aug, 2023</p> */}
+                    <p className='pro-des'>• The goal is to enable users to input any text and obtain an audio output that represents the text naturally.
+                      <br></br>
+                      • Tech stack used are HTML, CSS, and JavaScript.</p>
+                    <button type="button" className="btn btn-light" id="btn">LINK</button>
+                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                  </div>
+                </li>
+
+                <li>
+                  <div className="pro">
+                    <p className='pro-role'>
+                      Frontend Developer, Big Data Center Of Excellence
+                    </p>
+                    {/* <p className='pro-date'>Oct, 2021 - Dec, 2021</p> */}
+                    <p className='pro-des'>• A project that transforms text by adding or removing elements to improve clarity or convey a specific message.
+                      <br></br>
+                      • Tech stack used are HTML, CSS, and JavaScript.</p>
+                    <button type="button" className="btn btn-light" id="btn">LINK</button>
+                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+
+            {/* LEFT DIV */}
+            <div className='project-box-left'>
+              <img src={projectPic} alt={"pic"} className='project-img' />
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }
--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,13 +1,66 @@
-import React from 'react'
+// import React from 'react'
+// import './Skill.css';
+
+// const Skill = () => {
+//     return (
+//         <div>
+//             {/* SKILL */}
+//         </div>
+//     )
+// }
+
+// export default Skill
+
+
+import React from "react";
 import './Skill.css';
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+    DiJavascript1,
+    DiReact,
+    DiNodejs,
+    DiBootstrap,
+    DiPython,
+    DiDjango, DiJava, DiRasberryPi
+} from "react-icons/di";
+import { SiJupyter } from "react-icons/si";
 
 const Skill = () => {
     return (
-        <div>
-            SKILL
-        </div>
-    )
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            <Col xs={4} md={2} className="tech-icons">
+                <CgCPlusPlus />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiJavascript1 />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiNodejs />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiReact />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiPython />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiDjango />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiJupyter />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiJava />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiBootstrap />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiRasberryPi />
+            </Col>
+        </Row>
+    );
 }
 
-export default Skill
-
+export default Skill;
