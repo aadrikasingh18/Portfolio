e57151ab1b3febb34cd5bdf5afa4549871998df2
aadrikasingh18 <aadrikas16@gmail.com>
2024-03-01 01:20:13 +0530

    Updates

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,72 +1,70 @@
 import React from 'react'
 import './Project.css';
 import projectPic from '../images/project.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Project = () => {
   return (
     <>
-      <section>
-        <div className='project'>
-          <h2 className='project-heading'>
-            PROJECTS
-          </h2>
+      <Container className='project'>
+        <h2 className='project-heading'>
+          PROJECTS
+        </h2>
+        <Row>
+          {/* RIGHT DIV */}
+          <Col lg={{ span: 5, order: 1 }} sm={{ span: 12, order: 1 }} className='project-box-right'>
+            <ul className='project-list'>
+              <li>
+                <div className="pro">
+                  <p className='pro-role'>
+                    VEWRITE: A CONTENT MANAGEMENT TOOL
+                  </p>
+                  <p className='pro-des'>• It has been crafted to empower writers in content creation and management, all while fostering seamless collaboration with fellow writers.
+                    <br></br>
+                    • Tech stack used are ReactJs, Tailwind CSS, Firebase, and Sentry.</p>
+                  <button type="button" className="btn btn-light" id="btn">LINK</button>
+                  <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                </div>
+              </li>
 
-          <div className='project-container'>
-            {/* RIGHT DIV */}
-            <div className='project-box-right'>
-              <ul className='project-list'>
-                <li>
-                  <div className="pro">
-                    <p className='pro-role'>
-                      VEWRITE: A CONTENT MANAGEMENT TOOL
-                    </p>
-                    {/* <p className='pro-date'>Aug, 2023 - Oct, 2023</p> */}
-                    <p className='pro-des'>• It has been crafted to empower writers in content creation and management, all while fostering seamless collaboration with fellow writers.
-                      <br></br>
-                      • Tech stack used are ReactJs, Tailwind CSS, Firebase, and Sentry.</p>
-                    <button type="button" className="btn btn-light" id="btn">LINK</button>
-                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
-                  </div>
-                </li>
+              <li>
+                <div className="pro">
+                  <p className='pro-role'>
+                    TEXT TO SPEECH CONVERTER
+                  </p>
+                  <p className='pro-des'>• The goal is to enable users to input any text and obtain an audio output that represents the text naturally.
+                    <br></br>
+                    • Tech stack used are HTML, CSS, and JavaScript.</p>
+                  <button type="button" className="btn btn-light" id="btn">LINK</button>
+                  <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                </div>
+              </li>
 
-                <li>
-                  <div className="pro">
-                    <p className='pro-role'>
-                      TEXT TO SPEECH CONVERTER
-                    </p>
-                    {/* <p className='pro-date'>July, 2023 - Aug, 2023</p> */}
-                    <p className='pro-des'>• The goal is to enable users to input any text and obtain an audio output that represents the text naturally.
-                      <br></br>
-                      • Tech stack used are HTML, CSS, and JavaScript.</p>
-                    <button type="button" className="btn btn-light" id="btn">LINK</button>
-                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
-                  </div>
-                </li>
+              <li>
+                <div className="pro">
+                  <p className='pro-role'>
+                    Frontend Developer, Big Data Center Of Excellence
+                  </p>
 
-                <li>
-                  <div className="pro">
-                    <p className='pro-role'>
-                      Frontend Developer, Big Data Center Of Excellence
-                    </p>
-                    {/* <p className='pro-date'>Oct, 2021 - Dec, 2021</p> */}
-                    <p className='pro-des'>• A project that transforms text by adding or removing elements to improve clarity or convey a specific message.
-                      <br></br>
-                      • Tech stack used are HTML, CSS, and JavaScript.</p>
-                    <button type="button" className="btn btn-light" id="btn">LINK</button>
-                    <button type="button" className="btn btn-light" id="btn">GITHUB</button>
-                  </div>
-                </li>
+                  <p className='pro-des'>• A project that transforms text by adding or removing elements to improve clarity or convey a specific message.
+                    <br></br>
+                    • Tech stack used are HTML, CSS, and JavaScript.</p>
+                  <button type="button" className="btn btn-light" id="btn">LINK</button>
+                  <button type="button" className="btn btn-light" id="btn">GITHUB</button>
+                </div>
+              </li>
+            </ul>
+          </Col>
 
-              </ul>
-            </div>
+          {/* LEFT DIV */}
+          <Col lg={{ span: 7, order: 2 }} sm={{ span: 12, order: 2 }} className='project-box-left'>
+            <img src={projectPic} alt={"pic"} className='project-img' />
+          </Col>
+        </Row>
+      </Container>
 
-            {/* LEFT DIV */}
-            <div className='project-box-left'>
-              <img src={projectPic} alt={"pic"} className='project-img' />
-            </div>
-          </div>
-        </div>
-      </section>
     </>
   )
 }
